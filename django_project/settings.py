d3872183ed06d83fac8deb4c1bfa38865afa914f
Ritam Santra <101863276+DwightRitam@users.noreply.github.com>
2023-07-09 12:25:44 +0530

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,13 +126,11 @@ STATICFILES_DIR = (
     os.path.join(BASE_DIR, "public/static"),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
