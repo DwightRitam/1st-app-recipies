235e1138715089e09bb23ad341c702c8c5968fd9
Ritam Santra <101863276+DwightRitam@users.noreply.github.com>
2023-07-09 12:47:45 +0530

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,15 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static/"] 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 import os
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles','staticfiles_build', 'static')
 STATICFILES_DIR = (
     os.path.join(BASE_DIR, "public/static"),
 )
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 MEDIA_URL = '/media/'
