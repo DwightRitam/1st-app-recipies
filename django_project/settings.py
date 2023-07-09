d2aac71ecf8fdcf1c88e137a52edb31b3cc827e7
Ritam Santra <101863276+DwightRitam@users.noreply.github.com>
2023-07-09 13:42:50 +0530

    database and favicon changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u1@gq@7=+c3pjw7y4(1fq4hp@ifwm!!(spbz!b^yze$k-&2iq&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'grxClnzWACPMdIuVfZ6S',
+        'HOST': 'containers-us-west-193.railway.app',
+        'PORT': '6017',
+    }
 }
 
 
